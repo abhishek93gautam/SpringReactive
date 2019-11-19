ce7c724e270ba43401c0db0e07271f3bc774c3c4
Abhishek <abugautam@gmail.com>
2019-11-19 08:37:13 +0530

    Added more test cases to ItemReactiveRepositoryTest

--- src/test/java/com/learnreactivespring/repository/ItemReactiveRepositoryTest.java
+++ src/test/java/com/learnreactivespring/repository/ItemReactiveRepositoryTest.java
@@ -8,8 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import java.util.Arrays;
@@ -17,12 +19,13 @@ import java.util.List;
 
 @DataMongoTest
 @RunWith(SpringRunner.class)
+@DirtiesContext
 public class ItemReactiveRepositoryTest {
 
     @Autowired
     ItemReactiveRepository itemReactiveRepository;
 
-    List<Item> itemList = Arrays.asList(new Item(null,"Samsumg TV",400.0),
+    List<Item> itemList = Arrays.asList(new Item(null,"Samsung TV",400.0),
             new Item(null,"LG TV",440.0),
             new Item(null,"Apple Watch",299.99),
             new Item(null,"Beats Headphones",149.99),
@@ -53,7 +56,79 @@ public class ItemReactiveRepositoryTest {
     public void getItemById(){
         StepVerifier.create(itemReactiveRepository.findById("ABC"))
                 .expectSubscription()
-                .expectNextMatches((item-> item.getDescription().equals("Bose Headphones")))
+                .expectNextMatches((item)-> item.getDescription().equals("Bose Headphones"))
+                .verifyComplete();
+    }
+
+    @Test
+    public void findByDescription(){
+        StepVerifier.create(itemReactiveRepository.findByDescription("Apple Watch"))
+                            .expectSubscription()
+                            .expectNextCount(1)
+                            .verifyComplete();
+    }
+
+    @Test
+    public void saveItem(){
+
+        Item item = new Item(null,"Google Home Mini",30.00);
+        Mono<Item> savedItem =  itemReactiveRepository.save(item);
+
+        StepVerifier.create(savedItem.log("SavedItem  : "))
+                    .expectSubscription()
+                    .expectNextMatches((item1)->item1.getId()!=null && item1.getDescription().equals("Google Home Mini"))
+                    .verifyComplete();
+    }
+
+    @Test
+    public void updateItem(){
+
+        double newPrice = 520.0;
+        Mono<Item> updatedItem =  itemReactiveRepository.findByDescription("LG TV")
+                                .map(item->{
+                                     item.setPrice(newPrice);
+                                     return item;
+                                })
+                                .flatMap(item->{
+                                    return itemReactiveRepository.save(item);
+                                });
+
+        StepVerifier.create(updatedItem.log("UpdatedItem :"))
+                    .expectSubscription()
+                    .expectNextMatches(item->item.getPrice()==520.0)
+                    .verifyComplete();
+    }
+
+    @Test
+    public void deleteItemById(){
+        Mono<Void> deletedItem =  itemReactiveRepository.findById("ABC")
+                                .map(Item::getId)
+                                .flatMap((id)->{
+                                    return itemReactiveRepository.deleteById(id);
+                                });
+
+        StepVerifier.create(deletedItem)
+                    .expectSubscription()
+                    .verifyComplete();
+
+        StepVerifier.create(itemReactiveRepository.findAll())
+                    .expectNextCount(4)
+                    .verifyComplete();
+    }
+
+    @Test
+    public void deleteItem(){
+        Mono<Void> deletedItem =  itemReactiveRepository.findByDescription("LG TV")
+                .flatMap((item)->{
+                    return itemReactiveRepository.delete(item);
+                });
+
+        StepVerifier.create(deletedItem)
+                .expectSubscription()
+                .verifyComplete();
+
+        StepVerifier.create(itemReactiveRepository.findAll())
+                .expectNextCount(4)
                 .verifyComplete();
     }
 }
