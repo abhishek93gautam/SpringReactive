62689306860f92faea87fa0328a15cb48109462a
Abhishek <abugautam@gmail.com>
2019-11-28 08:37:37 +0530

    Added post delete and update end points to Item repository

--- src/main/java/com/learnreactivespring/controller/v1/ItemController.java
+++ src/main/java/com/learnreactivespring/controller/v1/ItemController.java
@@ -5,9 +5,11 @@ import com.learnreactivespring.document.Item;
 import com.learnreactivespring.repository.ItemReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @Slf4j
@@ -20,4 +22,36 @@ public class ItemController {
     public Flux<Item> getAllItems(){
         return itemReactiveRepository.findAll();
     }
+
+    @GetMapping(ItemConstants.ITEM_END_POINT_V1+"/{id}")
+    public Mono<ResponseEntity<Item>> getOneItem(@PathVariable String id){
+        return itemReactiveRepository.findById(id)
+                                    .map((item)-> new ResponseEntity<>(item, HttpStatus.OK))
+                                    .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping(ItemConstants.ITEM_END_POINT_V1)
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<Item> createItem(@RequestBody Item item){
+        return itemReactiveRepository.save(item);
+    }
+
+    @DeleteMapping(ItemConstants.ITEM_END_POINT_V1+"/{id}")
+    public Mono<Void> deleteItem(@PathVariable String id){
+        return itemReactiveRepository.deleteById(id);
+    }
+
+    @PutMapping(ItemConstants.ITEM_END_POINT_V1+"/{id}")
+    public Mono<ResponseEntity<Item>> updateItem(@PathVariable String id,@RequestBody Item item){
+        return itemReactiveRepository.findById(id)
+                                .flatMap((currentItem)->{
+                                    currentItem.setPrice(item.getPrice());
+                                    currentItem.setDescription(item.getDescription());
+                                    return itemReactiveRepository.save(currentItem);
+                                })
+                                .map(updatedItem->new ResponseEntity<>(updatedItem,HttpStatus.OK))
+                                .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+
 }
--- src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
+++ src/test/java/com/learnreactivespring/controller/v1/ItemControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
 import static org.junit.Assert.*;
@@ -97,6 +98,78 @@ public class ItemControllerTest {
                     .verifyComplete();
     }
 
+    @Test
+    public void getOneItem(){
+
+        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"),"ABC")
+                            .exchange()
+                            .expectStatus().isOk()
+                            .expectBody()
+                            .jsonPath("$.price",199.99);
+    }
+
+    @Test
+    public void getOneItem_notFound(){
+
+        webTestClient.get().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"),"DEF")
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
+    public void createItem(){
+        Item item = new Item(null,"Iphone X",999.99);
+        webTestClient.post().uri(ItemConstants.ITEM_END_POINT_V1)
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .body(Mono.just(item),Item.class)
+                            .exchange()
+                            .expectStatus().isCreated()
+                            .expectBody()
+                            .jsonPath("$.id").isNotEmpty()
+                            .jsonPath("$.description").isEqualTo("Iphone X")
+                            .jsonPath("$.price").isEqualTo(999.99);
+    }
+
+    @Test
+    public void deleteItem(){
+
+        webTestClient.delete().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"),"ABC")
+                                .accept(MediaType.APPLICATION_JSON)
+                                .exchange()
+                                .expectStatus().isOk()
+                                .expectBody(Void.class);
+    }
+
+    @Test
+    public void updateItem(){
+
+        double newPrice = 129.99;
+        Item item = new Item(null,"Beats Headphones",newPrice);
+
+        webTestClient.put().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"),"ABC")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .accept(MediaType.APPLICATION_JSON)
+                            .body(Mono.just(item),Item.class)
+                            .exchange()
+                            .expectStatus().isOk()
+                            .expectBody()
+                            .jsonPath("$.price",129.99);
+    }
+
+    @Test
+    public void updateItem_notFound(){
+
+        double newPrice = 129.99;
+        Item item = new Item(null,"Beats Headphones",newPrice);
+
+        webTestClient.put().uri(ItemConstants.ITEM_END_POINT_V1.concat("/{id}"),"DEF")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .body(Mono.just(item),Item.class)
+                .exchange()
+                .expectStatus().isNotFound();
+
+    }
 
 
 }
