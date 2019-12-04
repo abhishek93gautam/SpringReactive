3d7e08b6b42f576380432d12307d0ddc7c8b9fa4
Abhishek <abugautam@gmail.com>
2019-12-04 08:16:01 +0530

    Added functional stream end points for infinite streams

--- src/main/java/com/learnreactivespring/router/ItemRouter.java
+++ src/main/java/com/learnreactivespring/router/ItemRouter.java
@@ -33,4 +33,10 @@ public class ItemRouter {
         return RouterFunctions.route(GET("fun/runtimeException").and(accept(MediaType.APPLICATION_JSON)),
                 itemHandler::itemsEx);
     }
+
+    @Bean
+    public RouterFunction<ServerResponse> itemStreamRoute(ItemHandler itemHandler){
+        return RouterFunctions.route(GET(ItemConstants.ITEM_STREAM_FUNCTIONAL_END_POINT_V1).and(accept(MediaType.APPLICATION_JSON)),
+                itemHandler::itemsStream);
+    }
 }
